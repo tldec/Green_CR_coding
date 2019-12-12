145a5bb4731d592ad8b79cdc1a4414e8ece05250
tldec <45587307+tldec@users.noreply.github.com>
2019-12-12 23:49:16 +0800

    添加了utility收敛时的参数备注

--- src/code/config.py
+++ src/code/config.py
@@ -23,8 +23,34 @@ EH_max=0.2
 numOfL=14
 numOfN=15
 numOfCH=4
-# weights=np.array([800])
-weights=np.array(range(200,800,200))
+#
+############### mark ###########
+# epsilons=np.array([0.8])
+# timeSlots=5000
+# tau=60
+# P_T=0.94
+# # J/bit
+# P_H=0.069
+# P_max=3.5
+# dataArrival_max=10
+# gain=15
+# band=9e8
+# bandWidth = 1
+# initCapacityRate = 0.75
+# EH_max=0.2
+# numOfL=14
+# numOfN=15
+# numOfCH=4
+# maxSlop = 1
+# beta = 1
+# noise = 1e-5
+# # 覆盖半径(m)
+# radius = 80
+# para = P_T*gain*gain*3e16/np.power((4*np.pi*band),2)
+# weights=np.array(range(200,8000,200))
+# 在上述条件确定的时候，weight最大到8000时utility开始收敛
+############### mark over ###########
+weights=np.array(range(200,8000,200))
 maxSlop = 1
 beta = 1
 noise = 1e-5
